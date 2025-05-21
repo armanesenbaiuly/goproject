b4be6e1463ca31fff9befa5d4226f644386a69c6
armanesenbaiuly <arman.sagyndyk@narxoz.kz>
2025-05-21 16:04:24 +0500

    commit

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/armanesenbaiuly/go-rest-gorm/internal/handlers"
+	"github.com/armanesenbaiuly/go-rest-gorm/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,11 +11,17 @@ func SetupRoutes() *gin.Engine {
 
 	routes := r.Group("api/v1/tasks")
 	{
-		routes.POST("/", handlers.Create)
+		routes.POST("/", middleware.JWTMiddleware(), handlers.Create)
 		routes.PUT("/:id", handlers.Update)
 		routes.GET("/", handlers.GetAll)
 		routes.GET("/:id", handlers.GetById)
 		routes.DELETE("/:id", handlers.DeleteById)
+	}
+
+	authRoutes := r.Group("api/v1/auth")
+	{
+		authRoutes.POST("/register", middleware.JWTMiddleware(), handlers.Create)
+		authRoutes.POST("/login", handlers.Update)
 	}
 
 	return r
